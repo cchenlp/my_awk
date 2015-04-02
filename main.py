a32be643c12c775ca725016e158a9317e2f8da7b
mint <mint@example.com>
2015-04-02 17:06:40 +0800

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 import re
 input_file=open(sys.argv[1])
+# Change the regex to what you want and make the groups 
 regex=re.compile(r'[0-9]+.*<([0-9]+\.[0-9]+)>')
 for line in input_file:
         m=regex.match(line)
